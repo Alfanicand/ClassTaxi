c4be83a82a11a20348dcf2808b94a57daba858de
Alfanicand <127711466+Alfanicand@users.noreply.github.com>
2023-03-21 11:01:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classtaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Agus";
+            taxi.OnDuty = true;
+            taxi.NumPassanger = 10;
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassanger();
+            taxi.DropOffPasanger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classtaxi
+{
+     public class Taxi
+    {
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassanger { get; set; }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name: {0}", DriverName);
+                if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty: Yes");
+            }
+                else if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty: No");
+            }
+            Console.WriteLine("Number Of Passanger: {0}", NumPassanger);
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("\n{0} Sedang Menjemput Penumpang", DriverName);
+        }
+        public void DropOffPasanger()
+        {
+            Console.WriteLine("{0} Selsai Mengantar Penumpang", DriverName);
+        }
+    }
+}
